e9a354a8b6fb361e68e11652fead7d41af730b74
bfrazier <bthehippie2@gmail.com>
2023-04-19 15:13:03 -0400

    simple express server setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('this will be the homepage!'));
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
